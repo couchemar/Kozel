203b5c1aa490d9505bb02ccae6ebd3aeb3e1e4d9
Andrey Pavlov <couchemar@yandex.ru>
2012-08-07 18:49:17 +0400

    Change .hrl file add cards.erl

---
+++ src/cards.erl
@@ -0,0 +1,20 @@
+-module(cards).
+-include_lib("include/cards.hrl").
+
+-export([compare/2]).
+
+compare(Card1, Card2) ->
+    P1 = get_power(Card1),
+    P2 = get_power(Card2),
+
+    C1 = get_cost(Card1),
+    C2 = get_cost(Card2),
+    % Возвращаем {Card, Power, Cost}
+    {Card1, P1, C1}.
+
+
+get_power(_) ->
+    0.
+
+get_cost(_) ->
+    0.
