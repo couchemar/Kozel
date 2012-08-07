abdcb05f69c000ceda5e100d68b6fe2d167ab12d
Andrey Pavlov <couchemar@yandex.ru>
2012-08-07 19:07:46 +0400

    Added cards test FAILED

--- src/cards.erl
+++ src/cards.erl
@@ -12,7 +12,6 @@ compare(Card1, Card2) ->
     % Возвращаем {Card, Power, Cost}
     {Card1, P1, C1}.
 
-
 get_power(_) ->
     0.
 
---
+++ test/cards_test.erl
@@ -0,0 +1,7 @@
+-module(cards_test).
+-include_lib("eunit/include/eunit.hrl").
+
+compare_test_() ->
+    [?_assertEqual({{"♣", "Q"}, 14, 3},
+                  cards:compare({"♣", "Q"}, {"♠", "Q"}))
+    ].
