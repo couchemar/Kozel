fbf0b5dd4fbe610160001fea9e07a8a25e40ced5
Andrey Pavlov <couchemar@yandex.ru>
2012-08-13 16:42:25 +0400

    Добавили тесты сравнения козырей и обычных карт

--- test/cards_test.erl
+++ test/cards_test.erl
@@ -3,22 +3,29 @@
 
 compare_trumps_test_() ->
     [?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♣", "Q"}, {"♠", "Q"})),
+                   cards:compare({"♣", "Q"}, {"♠", "Q"})),
      ?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♣", "Q"}, {"♥", "Q"})),
+                   cards:compare({"♣", "Q"}, {"♥", "Q"})),
      ?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♣", "Q"}, {"♦", "Q"})),
+                   cards:compare({"♣", "Q"}, {"♦", "Q"})),
      ?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♠", "Q"}, {"♣", "Q"})),
+                   cards:compare({"♠", "Q"}, {"♣", "Q"})),
      ?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♥", "Q"}, {"♣", "Q"})),
+                   cards:compare({"♥", "Q"}, {"♣", "Q"})),
      ?_assertEqual({{"♣", "Q"}, 14, 3},
-                  cards:compare({"♦", "Q"}, {"♣", "Q"}))
+                   cards:compare({"♦", "Q"}, {"♣", "Q"}))
     ].
 
 compare_not_trumps_test_() ->
     [?_assertEqual({{"♣", "A"}, 0, 11},
-                  cards:compare({"♣", "A"}, {"♠", "10"})),
+                   cards:compare({"♣", "A"}, {"♠", "10"})),
      ?_assertEqual({{"♠", "A"}, 0, 11},
-                  cards:compare({"♣", "10"}, {"♠", "A"}))
+                   cards:compare({"♣", "10"}, {"♠", "A"}))
+    ].
+
+compare_trumps_and_not_trumps_test_() ->
+    [?_assertEqual({{"♣", "Q"}, 14, 3},
+                   cards:compare({"♣", "Q"}, {"♠", "10"})),
+     ?_assertEqual({{"♣", "Q"}, 14, 3},
+                   cards:compare({"♠", "10"}, {"♣", "Q"}))
     ].
