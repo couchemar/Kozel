b764736a0d03e4540e2e5b64631c2e0b8069fe9c
Andrey Pavlov <couchemar@yandex.ru>
2012-08-13 17:30:22 +0400

    Добавили coverage

--- test/cards_test.erl
+++ test/cards_test.erl
@@ -20,7 +20,9 @@ compare_not_trumps_test_() ->
     [?_assertEqual({{"♣", "A"}, 0, 11},
                    cards:compare({"♣", "A"}, {"♠", "10"})),
      ?_assertEqual({{"♠", "A"}, 0, 11},
-                   cards:compare({"♣", "10"}, {"♠", "A"}))
+                   cards:compare({"♣", "10"}, {"♠", "A"})),
+     ?_assertEqual({{"♣", "A"}, 0, 11},
+                   cards:compare({"♣", "A"}, {"♠", "A"}))
     ].
 
 compare_trumps_and_not_trumps_test_() ->
@@ -29,3 +31,4 @@ compare_trumps_and_not_trumps_test_() ->
      ?_assertEqual({{"♣", "Q"}, 14, 3},
                    cards:compare({"♠", "10"}, {"♣", "Q"}))
     ].
+
