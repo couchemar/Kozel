b7a88ed7ad171fe90c2011543718d728b410e5ae
Andrey Pavlov <couchemar@yandex.ru>
2012-08-29 15:00:57 +0400

    Добавил два пустых состояния

--- src/hand.erl
+++ src/hand.erl
@@ -1,9 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author Andrey Pavlov <couchemar@couchemar>
+%%% @copyright (C) 2012, Andrey Pavlov
+%%% @doc
+%%% State-machine отвечающий за состояние "руки" игрока.
+%%% @end
+%%% Created : 28 Aug 2012 by Andrey Pavlov <couchemar@couchemar>
+%%%-------------------------------------------------------------------
 -module(hand).
 
 -behaviour(gen_fsm).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% gen_fsm callbacks
 -export([init/1, state_name/2, state_name/3, handle_event/3,
@@ -11,46 +19,128 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {cards}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-start_link() ->
-    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Cards) ->
+    gen_fsm:start_link(?MODULE, [Cards], []).
 
 %%%===================================================================
 %%% gen_fsm callbacks
 %%%===================================================================
 
-init([]) ->
-    {ok, state_name, #state{}}.
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_fsm is started using gen_fsm:start/[3,4] or
+%% gen_fsm:start_link/[3,4], this function is called by the new
+%% process to initialize.
+%%
+%% @spec init(Args) -> {ok, StateName, State} |
+%%                     {ok, StateName, State, Timeout} |
+%%                     ignore |
+%%                     {stop, StopReason}
+%% @end
+%%--------------------------------------------------------------------
+init([Cards]) ->
+    {ok, idle, #state{cards=Cards}}.
 
-state_name(_Event, State) ->
-    {next_state, state_name, State}.
+idle(_Event, State) ->
+    {next_state, idle, State}.
 
-state_name(_Event, _From, State) ->
+turn(_Event, State) ->
+    {next_state, turn, State}.
+
+idle(_Event, _From, State) ->
     Reply = ok,
-    {reply, Reply, state_name, State}.
+    {reply, Reply, turn, State}.
 
+turn(_Event, _From, State) ->
+    Reply = ok,
+    {reply, Reply, idle, State}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_fsm receives an event sent using
+%% gen_fsm:send_all_state_event/2, this function is called to handle
+%% the event.
+%%
+%% @spec handle_event(Event, StateName, State) ->
+%%                   {next_state, NextStateName, NextState} |
+%%                   {next_state, NextStateName, NextState, Timeout} |
+%%                   {stop, Reason, NewState}
+%% @end
+%%--------------------------------------------------------------------
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_fsm receives an event sent using
+%% gen_fsm:sync_send_all_state_event/[2,3], this function is called
+%% to handle the event.
+%%
+%% @spec handle_sync_event(Event, From, StateName, State) ->
+%%                   {next_state, NextStateName, NextState} |
+%%                   {next_state, NextStateName, NextState, Timeout} |
+%%                   {reply, Reply, NextStateName, NextState} |
+%%                   {reply, Reply, NextStateName, NextState, Timeout} |
+%%                   {stop, Reason, NewState} |
+%%                   {stop, Reason, Reply, NewState}
+%% @end
+%%--------------------------------------------------------------------
 handle_sync_event(_Event, _From, StateName, State) ->
     Reply = ok,
     {reply, Reply, StateName, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_fsm when it receives any
+%% message other than a synchronous or asynchronous event
+%% (or a system message).
+%%
+%% @spec handle_info(Info,StateName,State)->
+%%                   {next_state, NextStateName, NextState} |
+%%                   {next_state, NextStateName, NextState, Timeout} |
+%%                   {stop, Reason, NewState}
+%% @end
+%%--------------------------------------------------------------------
 handle_info(_Info, StateName, State) ->
     {next_state, StateName, State}.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_fsm when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_fsm terminates with
+%% Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, StateName, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
 terminate(_Reason, _StateName, _State) ->
     ok.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, StateName, State, Extra) ->
+%%                   {ok, StateName, NewState}
+%% @end
+%%--------------------------------------------------------------------
 code_change(_OldVsn, StateName, State, _Extra) ->
     {ok, StateName, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
