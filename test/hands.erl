bcd826c381da59bf5db8e2abcbabe65fe13e0926
Andrey Pavlov <couchemar@yandex.ru>
2012-08-30 17:56:56 +0400

    Пытаемся писать тесты

--- test/hands.erl
+++ test/hands.erl
@@ -1,4 +1,17 @@
 -module(hands).
 -include_lib("eunit/include/eunit.hrl").
 
+hands_test_() ->
+    {setup,
+     fun start/0,
+     fun stop/1,
+     fun hand_test/1}.
 
+start() ->
+    ok.
+
+stop(_) ->
+    ok.
+
+hand_test(_) ->
+    ok.
